9ef9e413093cc88636a37c6f392ab181c23ddfbe
OlDashkevych <dashkevich672@gmail.com>
2021-09-23 12:27:31 +0300

    fix

---
+++ build/precache-manifest.e58e153d2cee4b692808e3ecd9953fad.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45dc609fb6e737f6da7b4ab74e0ad945",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ac6db86ab4f21203877c",
+    "url": "/static/css/4.6a05dc23.chunk.css"
+  },
+  {
+    "revision": "90904f70bb807e07e602",
+    "url": "/static/css/main.44c23598.chunk.css"
+  },
+  {
+    "revision": "e97b9bf0d7f480756f96",
+    "url": "/static/js/2.9a65e216.chunk.js"
+  },
+  {
+    "revision": "505dfa1ed2dd6704c255",
+    "url": "/static/js/3.d19032dc.chunk.js"
+  },
+  {
+    "revision": "ac6db86ab4f21203877c",
+    "url": "/static/js/4.834e60ef.chunk.js"
+  },
+  {
+    "revision": "90904f70bb807e07e602",
+    "url": "/static/js/main.f2a4e79f.chunk.js"
+  },
+  {
+    "revision": "68079e69816cad7fccdb",
+    "url": "/static/js/runtime-main.e2db2925.js"
+  }
+]);
